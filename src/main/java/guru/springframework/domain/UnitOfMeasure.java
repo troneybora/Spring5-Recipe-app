3df7cd8846281ea407c99f601cf5073d0714ccb6
Troney <troneybora@hotmail.com>
2020-05-23 00:21:02 +0530

    UOM Ingerdient,One to One

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,34 @@
+package guru.springframework.domain;
+
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+}
